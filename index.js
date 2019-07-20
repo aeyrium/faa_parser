11e630b233467ec27e254f780e87f690753c0d78
Rodney Aiglstorfer <rodney@Mechazilla.local>
2019-07-20 14:22:24 -0700

    removed

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 
 module.exports.Parser = require('./src/parser')
-module.exports.Aircraft = require('./src/mapping/aircraft')
-module.exports.AircraftMfr = require('./src/mapping/aircraftMfr')
-module.exports.EngineMfr = require('./src/mapping/engineMfr')
 module.exports.Airmen = require('./src/mapping/airmen')
 module.exports.Airport = require('./src/mapping/airport')
